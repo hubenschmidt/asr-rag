d66a3bfac93a739b4a42e236318a7bc0f481c5ad
William Hubenschmidt <whubenschmidt@gmail.com>
2026-02-19 20:18:51 -0500

    refactor main.go

--- main.go
+++ main.go
@@ -6,14 +6,77 @@ import (
 	"os"
 )
 
+type command struct {
+	usage string
+	run   func(args []string) error
+}
+
 func main() {
 	cfg, err := loadConfig("config.json")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "config: %v\n", err)
+		fmt.Fprintf(os.Stderr, "config: %v\n", err)
+		os.Exit(1)
+	}
+
+	commands := map[string]command{ // e.g. command["seed"]run // this is like Python dict[str, Command] or typescript Record<string, Command>
+		"seed": {
+			usage: "seed -- embed corpus and upsert to Qdrant",
+			run: func(args []string) error {
+				fmt.Println("seed: not yet implemented")
+				return nil
+			},
+		},
+		"transcribe": {
+			usage: "transcribe <file.wav> -- transcribe and correcta WAV file",
+			run: func(args []string) error {
+				fmt.Println("transcribe: not yet implemented")
+				return nil
+			},
+		},
+		"record": {
+			usage: "record [seconds] -- record from mic, transcribe, and correct",
+			run: func(args []string) error {
+				fmt.Println("record: not yet implemented")
+				return nil
+			},
+		},
+		"search": {
+			usage: "search <query> -- search Qdrant for similar Go terms",
+			run: func(args []string) error {
+				fmt.Println("search: not yet implemented")
+				return nil
+			},
+		},
+	}
+
+	// if insufficient args, exit program
+	if len(os.Args) < 2 {
+		printUsage(commands)
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	cmd, ok := commands[name]
+	if !ok {
+		fmt.Println("unknown command: " + name)
+		printUsage(commands)
+		os.Exit(1)
+	}
+	if err := cmd.run(os.Args[2:]); err != nil {
+		// fmt.Println(name + ": " + err.Error()) // Println does not unpack .Error() so it must be done explictly
+		fmt.Fprintf(os.Stderr, "%s: %v\n", name, err) // Fprintf uses format verbs
 		os.Exit(1)
 	}
 
 	fmt.Println(*cfg) // *cfg dereferences and prints {http://localhost:8178}
+}
+
+func printUsage(commands map[string]command) {
+	fmt.Println("usage: asr-rag <command> [args]")
+	fmt.Println("\n commands:")
+	for _, cmd := range commands {
+		fmt.Println("  " + cmd.usage)
+	}
 }
 
 type config struct {
